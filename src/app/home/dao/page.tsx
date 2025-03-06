4c6c022bd95b2bd1bf88c517d3444050c8aea7e9
Chaal Pritam <chaalpritam@gmail.com>
2025-03-06 22:22:22 +0530

    adding DAO page in home

---
+++ src/app/home/dao/page.tsx
@@ -0,0 +1,10 @@
+export default function DAO() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ textAlign: 'center' }}>
+                <p>Harbour AI Agent</p>
+                <a href="/home">click here to try the agent use /home</a>
+            </div>
+        </div>
+    );
+}
